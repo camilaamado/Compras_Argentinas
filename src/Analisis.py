0a72d8bb591064a52c98f0035fb1068742b88388
camilaamado <amadocamilaines@gmail.com>
2025-02-24 19:30:59 -0300

    Transformacion y limpieza de datos 1

--- src/Analisis.py
+++ src/Analisis.py
@@ -20,17 +20,26 @@ print(dataset.head())
 # Obtener información general del dataset (tipos de datos, valores nulos, etc.)
 print(dataset.info())
 
+# Verificar los tipos de datos
+print(dataset.dtypes)
+
+
 # Ver los valores nulos y el porcentaje de nulos por columna
 missing_values = dataset.isnull().sum()
 missing_percentage = (missing_values / len(dataset)) * 100
 print(pd.DataFrame({'Missing Values': missing_values, 'Percentage': missing_percentage}))
 
-# Verificar los tipos de datos
-print(dataset.dtypes)
+# Asegurarte de que no hay valores nulos
+print(dataset.isnull().sum())
+if dataset.isnull().sum().sum() == 0:
+    print("No hay valores nulos en el dataset.")
+else:
+    print("Hay valores nulos en el dataset.")   
 
-# Estadísticas descriptivas para datos numéricos
-print(dataset.describe())
+# categorías presentes en cada variable categórica y su frecuencia
+for col in dataset.select_dtypes(include=['object', 'category']).columns:
+    print(f"Frecuencia de categorías en {col}:")
+    print(dataset[col].value_counts())
+    print("-" * 50)
 
-# Estadísticas para datos categóricos
-print(dataset['Eğitim_Seviyesi'].value_counts())
 
